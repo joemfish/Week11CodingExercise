b90e889ec653d48873a1181fafa97342bd23270b
joemfish <joe.m.fish@gmail.com>
2023-09-21 14:17:24 -0400

    week 10 coding exercise

--- src/main/java/projects/ProjectsApp.java
+++ src/main/java/projects/ProjectsApp.java
@@ -13,7 +13,9 @@ public class ProjectsApp {
 	
 	//@formatter:off
 	private List<String> operations = List.of(
-			"1) Add a project"
+			"1) Add a project", 
+			"2) List projects",
+			"3) Select a project"
 			
 			
 			);		
@@ -22,6 +24,7 @@ public class ProjectsApp {
 	
 	private Scanner scanner = new Scanner(System.in);
 	private ProjectService projectService = new ProjectService();
+	private Project curProject;
 
 	public static void main(String[] args) {
 	
@@ -40,20 +43,46 @@ public class ProjectsApp {
 				case -1:
 					done = exitMenu();
 					break;
+					
 				default:
 					System.out.println("\n" + selection + " is not a valid selection.  Try again.");
 					break;
+					
 				case 1:
 					createProject();
 					break;
+					
+				case 2:
+					listProjects();
+					break;
+					
+				case 3:
+					selectProject();
+					break;
+					
 				}
 				}
-				
-			
-				catch(Exception e) {
+					catch(Exception e) {
 					System.out.println("\nError: " + e + " Try again.");
 			}
 		}
+	}
+
+	private void selectProject() {
+		listProjects();
+		
+		Integer projectId = getIntInput("Enter the project ID you wish to select");
+		
+		curProject = null;
+		
+		curProject = projectService.fetchByProjectId(projectId);
+	}
+
+	private void listProjects() {
+		List<Project> projects = projectService.fetchAllProjects();
+		System.out.println("\nProjects:");
+		
+		projects.forEach(project -> System.out.println("    " + project.getProjectId() + ": " + project.getProjectName()));
 	}
 
 	private void createProject() {
@@ -126,6 +155,12 @@ public class ProjectsApp {
 	private void printOperations() {
 		System.out.println("\nThese are the available selections.  Press the Enter key to quit:");
 		operations.forEach(line -> System.out.println("  " +  line));
+		
+		if (Objects.isNull(curProject)) {
+			System.out.println("\nYou are not working with a project");
+		} else {
+			System.out.println("\nYou are working with project: " + curProject);
+		}
 	}
 
 	private boolean exitMenu() {
